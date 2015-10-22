9f81972b6374c493f9b99051e972c7471af9c365
ardoric <ardoric@gmail.com>
2015-10-22 12:08:50 +0100

    decrypt command line parameters

--- src/ardo/decrypter/Main.java
+++ src/ardo/decrypter/Main.java
@@ -6,17 +6,22 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		String key = CryptoUtils.readKey("private.key");
-		
-		HSConf hsconf = new HSConf("server.hsconf");
-		for (Entry<String, String> entry : hsconf.entries()) {
-			
-			try {
-				System.out.println(entry.getKey() + " " + CryptoUtils.decrypt(key,entry.getValue()));
-			} catch (Exception e) {
-				System.out.println(entry.getKey() + " <error>");
+		if (args.length >= 1) {
+			for (String s: args) {
+				System.out.println(CryptoUtils.decrypt(key, s));
+				System.out.println();
 			}
-			
+		} else {
+			HSConf hsconf = new HSConf("server.hsconf");
+			for (Entry<String, String> entry : hsconf.entries()) {
+
+				try {
+					System.out.println(entry.getKey() + " " + CryptoUtils.decrypt(key,entry.getValue()));
+				} catch (Exception e) {
+					System.out.println(entry.getKey() + " <error>");
+				}
+
+			}
 		}
 	}
-
 }
